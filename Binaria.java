99cee1992b414e93c291b9741c736bcfa3e9e328
Caroline Pizzolato <caroline0broker0pizzolato@gmail.com>
2025-05-04 16:22:32 -0300

    Update Binaria.java
    Mudei toda a estrutura para atender os requisitos da atividade Estrutura de dados arvore binaria, agora temos pesquisa e deleta funcionais!!

--- Binaria.java
+++ Binaria.java
@@ -1,110 +1,53 @@
-package arvorebinaria.EstruturaDadosAtividadeArvore;
+package binaria;
 
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class Binaria {
-	private Nodo raiz; // não esquece que é tipo Nodo
+public class ArvoreBinaria {
+	private Nodo raiz;
 
-	public Binaria() {
-		this.raiz = null;
+	public ArvoreBinaria() {
+		this.setRaiz(null);
 	}
 
 	public Nodo getRaiz() {
-		return this.raiz;
+		return raiz;
 	}
 
 	public void setRaiz(Nodo raiz) {
 		this.raiz = raiz;
 	}
 
-	public void pesquisa(int valor, Nodo aux) {
-		if (aux != null ) {	
-				if(aux.getValor() == valor) {
-				
-				System.out.println("valor encontrado! " + valor);
-				return;
-				}
-				else if (valor < aux.getValor() ){
-					pesquisa(valor, aux.esquerda);
-				}
-				else {
-					pesquisa(valor, aux.getDireita());
-			}
-				
-		}
-			if (aux == null)
-				System.out.println("valor nao encontrado: " + valor);
-	}
-	// inserir nodo sem recursivo
-	public void insere(int valor) {
+	public void insereNaoRecursivo(int valor) {
 		Nodo novo = new Nodo(valor, null, null);
-		boolean achou = false;
-		if (raiz == null) {
+
+		if (this.raiz == null)
 			this.raiz = novo;
-			return;
-		}
-		// tenho que pensar na direita e esquerda
-		Nodo aux = this.raiz;
-		while (!achou) {
-			if (valor < aux.getValor()) {
-				// tem duas condições a esquerda pode ser null ou ja colocada
-				if (aux.getEsquerda() != null) {
-					aux = aux.getEsquerda();
+		else {
+			Nodo aux = this.raiz;
+			boolean inseriu = false;
+			while (inseriu == false) {
+				if (novo.getValor() < aux.getValor()) {
+					if (aux.getEsquerda() != null)
+						aux = aux.getEsquerda();
+					else {
+						aux.setEsquerda(novo);
+						inseriu = true;
+					}
 				} else {
-					aux.setEsquerda(novo);
-					achou = true;
-				}
-			} else {
-				if (aux.getDireita() != null) {
-					aux = aux.getDireita();
-
-				} else {
-					aux.setDireita(novo);
-					achou = true;
+					if (aux.getDireita() != null)
+						aux = aux.getDireita();
+					else {
+						aux.setDireita(novo);
+						inseriu = true;
+					}
 				}
 			}
-		} // fim do while
-	} // fim do metodo
-	/*Insere recursivo foi mudado, mas foi uma mudança simples, ao inves de ter Nodo novo, agora será new nodo (x,x,x)*/
-	public void insereRecursivo(Nodo aux, int valor) {
-		if (aux != null) {
-			// esquerda
-			if (valor < aux.getValor()) {
-
-				if (aux.getEsquerda() != null) {
-					insereRecursivo(aux.getEsquerda(), valor);
-				} else
-					aux.setEsquerda(new Nodo(valor, null, null););
-			} else {
-				if (aux.getDireita() != null) {
-					insereRecursivo(aux.getDireita(), valor);
-				} else
-					aux.setDireita(new Nodo(valor, null, null););
-			}
-		} else {
-			// direita
-			setRaiz(new Nodo(valor, null, null);); // this.raiz = novo;
-			// esse set raiz auxilia em algumas linhas
 		}
+
 	}
 
-	public void imprimeRecursiva(Nodo aux) {
-		int i = 0;
-		if (this.raiz == null) {
-			System.out.println("Lista vazia!");
-			return;
-		}
-		if (aux != null) {
-			
-			
-			System.out.println("Valores: " + aux.getValor());
-			imprimeRecursiva(aux.getEsquerda());
-			imprimeRecursiva(aux.getDireita());
-		}
-		
-		// se colocar else vai dar errado pq é recursivo
-	}
+	// atividade extra imprimir nao recursivo
 
 	public void imprimeNaoRec() {
 		Queue<Nodo> fila = new LinkedList<>();
@@ -121,65 +64,124 @@ public class Binaria {
 		}
 	}
 
-	/*
-	 * 1. DELETA FOLHA () 2. DELETA A RAIZ() 3. DELETA NETOS ()
-	 * 
-	 * 
-	 * 
-	 */
-	public void deleta(int valor) {
-		Nodo aux = this.raiz;
-		if (this.raiz == null) {
-			System.out.println("Lista vazia, não é possível pesquisar!");
-			return;
-		}
-		// relação de comparação == valor
+	public void pesquisa(int valor, Nodo aux) {
+		if (aux != null) {
+			if (aux.getValor() == valor) {
 
-		boolean achou = false;
-		while (aux != null) {
-			if (valor < aux.getValor()) {
-				// primeiro quem não tem filho
-				if (aux.getEsquerda() != null && aux.getEsquerda().getValor() == valor) {
-					Nodo esquerda = aux.getEsquerda();
-					if (esquerda.getEsquerda() == null && esquerda.getDireita() == null) {
-						aux.setEsquerda(null);
-						System.out.println("Valor deletado! " + valor + " este valor é uma folha!!!!");
-						achou = true;
-						break;
-					} else {
-						aux = aux.getEsquerda();
-					}
-				}
-				if (achou == false) {
-					System.out.println("Valor não encontrado " + valor);
-						return;
-				}
+				System.out.println("valor encontrado! " + valor);
+				return;
+			} else if (valor < aux.getValor()) {
+				pesquisa(valor, aux.esquerda);
 			} else {
-				if (aux.getDireita() != null && aux.getDireita().getValor() == valor) {
-					Nodo direita = aux.getDireita();
-					if (direita.getEsquerda() == null && direita.getDireita() == null) {
-						aux.setDireita(null);
-						System.out.println("Valor deletado! " + valor + " este valor é uma folha!!!!");
-						achou = true;
-						break;
-					} else {
-						System.out.println("Nó encontrado mas não é folha!");
-						break;
-					}
-				} else {
-					aux = aux.getDireita();
-				}
-
-				if (achou == false) {
-					System.out.println("Valor não encontrado " + valor);
-					return;
-				}
+				pesquisa(valor, aux.getDireita());
 			}
 
-		} // while
-
+		}
+		if (aux == null)
+			System.out.println("valor nao encontrado: " + valor);
 	}
-	/*Professora está fazendo o código de pesquisa*/
 
 	
+
+	public void insereRecursivo(Nodo aux, int valor) {
+		Nodo novo = new Nodo(valor, null, null);
+
+		if (aux != null) {
+			if (valor < aux.getValor()) {
+				if (aux.getEsquerda() != null)
+					insereRecursivo(aux.getEsquerda(), valor);
+				else
+					aux.setEsquerda(novo);
+			} else {
+				if (aux.getDireita() != null)
+					insereRecursivo(aux.getDireita(), valor);
+				else
+					aux.setDireita(novo);
+			}
+		} else
+			setRaiz(novo);
+	}
+
+	public void imprime(Nodo aux) {
+		if (aux != null) {
+			System.out.println(aux.getValor());
+			imprime(aux.getEsquerda());
+			imprime(aux.getDireita());
+		}
+	}
+	public void removerRecursivo(Nodo aux, Nodo pai, int valor) {
+	    if (aux == null) {
+	        System.out.println("Valor não encontrado!");
+	        return;
+	    }
+
+	    if (valor < aux.getValor()) {
+	        removerRecursivo(aux.getEsquerda(), aux, valor);
+	    } else if (valor > aux.getValor()) {
+	        removerRecursivo(aux.getDireita(), aux, valor);
+	    } else {
+	        // Valor encontrado — decidir qual remoção aplicar
+	        if (aux.getEsquerda() == null && aux.getDireita() == null) {
+	            removeFolha(aux, pai);
+	        } else if (aux.getEsquerda() != null && aux.getDireita() == null) {
+	            removeComFilhoEsquerda(aux, pai);
+	        } else if (aux.getEsquerda() == null && aux.getDireita() != null) {
+	            removeComFilhoDireita(aux, pai);
+	        } else {
+	            removeComDoisFilhos(aux);
+	        }
+	        System.out.println("Valor " + valor + " removido.");
+	    }
+	}
+
+	private void removeFolha(Nodo aux, Nodo pai) {
+	    if (pai == null) {
+	        setRaiz(null);
+	    } else if (pai.getEsquerda() == aux) {
+	        pai.setEsquerda(null);
+	    } else if (pai.getDireita() == aux) {
+	        pai.setDireita(null);
+	    }
+	}
+
+	private void removeComFilhoEsquerda(Nodo aux, Nodo pai) {
+	    if (pai == null) {
+	        setRaiz(aux.getEsquerda());
+	    } else if (pai.getEsquerda() == aux) {
+	        pai.setEsquerda(aux.getEsquerda());
+	    } else if (pai.getDireita() == aux) {
+	        pai.setDireita(aux.getEsquerda());
+	    }
+	}
+
+	private void removeComFilhoDireita(Nodo aux, Nodo pai) {
+	    if (pai == null) {
+	        setRaiz(aux.getDireita());
+	    } else if (pai.getEsquerda() == aux) {
+	        pai.setEsquerda(aux.getDireita());
+	    } else if (pai.getDireita() == aux) {
+	        pai.setDireita(aux.getDireita());
+	    }
+	}
+
+	private void removeComDoisFilhos(Nodo aux) {
+	    // Encontrar o sucessor (menor valor da subárvore direita)
+	    Nodo sucessor = aux.getDireita();
+	    Nodo paiSucessor = aux;
+
+	    while (sucessor.getEsquerda() != null) {
+	        paiSucessor = sucessor;
+	        sucessor = sucessor.getEsquerda();
+	    }
+
+	    aux.setValor(sucessor.getValor());
+
+	    // Agora removemos o sucessor (ele tem no máximo um filho à direita)
+	    if (sucessor.getDireita() != null) {
+	        removeComFilhoDireita(sucessor, paiSucessor);
+	    } else {
+	        removeFolha(sucessor, paiSucessor);
+	    }
+	}
+
 }
